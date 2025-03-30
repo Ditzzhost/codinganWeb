c442b7c29f031efbcbba332790827034fe0919c3
Ditzzhost <radittyachyawrwan@gmail.com>
2025-03-31 01:13:23 +0700

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function sendToWhatsApp() {
     let name = document.getElementById('name').value;
     let product = document.getElementById('product').value;
     let customText = document.getElementById('customText').value;
-
-    let message = `Halo, saya ingin membeli ${product}.\nNama: ${name}\nDeskripsi: ${customText}`;
-    let whatsappURL = `https://wa.me/62XXXXXXXXXX?text=${encodeURIComponent(message)}`;
+    let phone = `6283194451891`;
+    
+    let message = `✨ Assalamu'alaikum Bang!✨\n\nSaya Ingin Membeli:\nNama: ${name}\nProduk: ${product}\nDeskripsi: ${customText}`;
+    let whatsappURL = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 
     window.open(whatsappURL, '_blank');
 }
